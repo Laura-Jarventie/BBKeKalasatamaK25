ce45c7ee6d8ce7c87ccc61ba4ac33ed88d687885
unknown <laurajarventie@hotmail.com>
2025-02-19 18:15:43 +0200

    19.2

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-let BOARD_SIZE = 15
+let BOARD_SIZE = 20
 let board; //kenttä tallentaan tähän
 const cellSize = calculateCellSize();
 
@@ -43,6 +43,8 @@ function generateRandomBoard(){
         }
     }
 
+    generateObstacles(newBoard);
+
     return  newBoard;
 }
 
@@ -69,5 +71,44 @@ function generateRandomBoard(){
     }
 } 
 
+function generateObstacles(board){
 
-//saatiin tehtyä responsivisuus. kirj doc 11.
+    //lista esteistä koordinaattiparien listoina
+    const obstacles = [
+        [[0,0],[0,1],[1,0],[1,1]], // Square
+        [[0,0],[0,1],[0,2],[0,3]],  // I
+        [[0,0],[1,0],[2,0],[1,1]], // T
+        [[1,0],[2,0],[1,1],[0,2],[1,2]], // Z
+        [[1,0],[2,0],[0,1],[1,1]], // S
+        [[0,0],[1,0],[1,1],[1,2]], // L
+        [[0,2],[0,1],[1,1],[2,1]]  // J
+    ];
+
+    //valitaan muutama paikka esteille pelikentältä
+    //huom kovakoodatut x ja y, eli katso että palikat mahtuu kentälle
+    const positions = [
+            { startX: 2, startY: 2 },
+            { startX: 8, startY: 2 },
+            { startX: 4, startY: 8 },
+            { startX: 3, startY: 16 },
+            { startX: 10, startY: 10 },
+            { startX: 12, startY: 5 },
+            { startX: 12, startY: 10 },
+            { startX: 16, startY: 10 },
+            { startX: 13, startY: 14 }
+       
+    ]
+
+    //käydään läpi valitut paikat ja arvotaan niihin esteet
+    positions.forEach(pos => {
+        const randomObstacle = obstacles[Math.floor(Math.random()* obstacles.length)];
+        placeObstacle(board, randomObstacle, pos.startX, pos.startY);
+    });
+}
+
+function placeObstacle(board, obstacle, startX, startY){
+    for (coordinatePair of obstacle){
+        [x,y] = coordinatePair;
+        board[startX + y][startY + x] = 'W'
+    }
+}
